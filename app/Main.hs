16ef84187954e119e743c817259e00e75a349df9
emilylo2 <68090204+emilylo2@users.noreply.github.com>
2023-12-17 01:05:45 -0500

    mutable array, works somtimes

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,107 @@
 import Lib
 import System.Environment (getArgs, getProgName)
+import Data.Array
+import Control.Monad (forM_)
+import Data.IORef
+import Control.Parallel.Strategies (rpar, parMap)
+
+
+data Scoring = Scoring
+  { matchScore :: Int
+  , mismatchPenalty :: Int
+  , gapPenalty :: Int
+  }
+
+score :: Scoring -> Char -> Char -> Int
+score scoring a b
+  | a == b    = matchScore scoring
+  | otherwise = -mismatchPenalty scoring
+
+type MutableArray = IORef (Array (Int, Int) Int)
+
+printImmutableArray :: Array (Int, Int) Int -> IO ()
+printImmutableArray arr = do
+  putStrLn "ran"
+  let (_, (n, m)) = bounds arr
+
+  forM_ [0..n] $ \i -> do
+    forM_ [0..m] $ \j -> do
+      putStr $ show (arr ! (i, j)) ++ "\t"
+    putStrLn ""
+
+initializeMutableArray :: Int -> IO MutableArray
+initializeMutableArray n = newIORef $ listArray ((0, 0), (n-1, n-1)) $ repeat 0
+
+
+updateMutableArray :: MutableArray -> (Int, Int) -> Int -> IO ()
+updateMutableArray mutableArray index newValue = do
+  oldArray <- readIORef mutableArray
+  let newArray = oldArray // [(index, newValue)]
+  writeIORef mutableArray newArray
+
+readIndex :: MutableArray -> (Int, Int) -> IO Int
+readIndex mutableArray index = do
+  currentArray <- readIORef mutableArray
+  return (currentArray ! index)
+
+antidiagonalIndices :: Int -> [[(Int, Int)]]
+antidiagonalIndices n =
+  [ [(i, k - i) | i <- [0..k], k - i < n, k - i >= 0 && i < n] | k <- [0..2*(n-1)] ]
+
+adiagonal :: Scoring -> String -> String -> IO MutableArray
+adiagonal scoring s1 s2 = do
+  let n = length s1
+      m = length s2
+  mutableArray <- initializeMutableArray n
+  diagonals <- return $ antidiagonalIndices n
+
+  forM_ diagonals $ \diag -> do
+    scores <- readIORef mutableArray
+    forM_ diag $ \(i, j) -> do
+      let newScore
+            | i == 0 = -j * gapPenalty scoring
+            | j == 0 = -i * gapPenalty scoring
+            | otherwise = maximum
+                            [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
+                            , scores ! (i, j-1) - gapPenalty scoring
+                            , scores ! (i-1, j) - gapPenalty scoring
+                            ]
+      updateMutableArray mutableArray (i, j) newScore
+
+  return mutableArray
+
+  -- Update the type signature
+traceback :: MutableArray -> String -> String -> IO (String, String)
+traceback mutableArray s1 s2 = do
+  scores <- readIORef mutableArray
+  let (n, m) = snd $ bounds scores
+
+  let go (i, j) (align1, align2)
+        | i == 0 && j == 0 = (align1, align2)
+        | i == 0 =
+          let newAlign1 = '-':align1
+              newAlign2 = s2 !! (j-1) : align2
+          in go (i, j-1) (newAlign1, newAlign2)
+        | j == 0 =
+          let newAlign1 = s1 !! (i-1) : align1
+              newAlign2 = '-':align2
+          in go (i-1, j) (newAlign1, newAlign2)
+        | scores ! (i, j) == scores ! (i-1, j) - 1 =
+          let newAlign1 = s1 !! (i-1) : align1
+              newAlign2 = '-' : align2
+          in go (i-1, j) (newAlign1, newAlign2)
+        | scores ! (i, j) == scores ! (i, j-1) - 1 =
+          let newAlign1 = '-':align1
+              newAlign2 = s2 !! (j-1) : align2
+          in go (i, j-1) (newAlign1, newAlign2)
+        | otherwise =
+          let newAlign1 = s1 !! (i-1) : align1
+              newAlign2 = s2 !! (j-1) : align2
+          in go (i-1, j-1) (newAlign1, newAlign2)
+
+  return $ go (n, m) ("", "")
+
+
 
 main :: IO ()
 main = do 
@@ -12,29 +114,31 @@ main = do
       putStrLn "Starting alignment..."
       case method of
         "adiagonal" -> do
-          let matrix = adiagonal schema seq1 seq2
-              (alignd1, alignd2) = traceback matrix seq1 seq2
+          matrix <- adiagonal schema seq1 seq2
+          immutableArray <- readIORef matrix
+          printImmutableArray immutableArray
+          (alignd1, alignd2) <- traceback matrix seq1 seq2
           putStrLn "Alignment is done! Writing aligned sequences to files"
           let outputFile = "./output/output.txt"
           writeFile outputFile $ unlines [alignd1, alignd2]
-        "row" -> do
-          let matrix = row schema seq1 seq2
-              (alignd1, alignd2) = traceback matrix seq1 seq2
-          putStrLn "Alignment is done! Writing aligned sequences to files"
-          let outputFile = "./output/output.txt"
-          writeFile outputFile $ unlines [alignd1, alignd2]
-        "column" -> do
-          let matrix = column schema seq1 seq2
-              (alignd1, alignd2) = traceback matrix seq1 seq2
-          putStrLn "Alignment is done! Writing aligned sequences to files"
-          let outputFile = "./output/output.txt"
-          writeFile outputFile $ unlines [alignd1, alignd2]
-        "sequential" -> do
-          let matrix = sequential schema seq1 seq2
-              (alignd1, alignd2) = traceback matrix seq1 seq2
-          putStrLn "Alignment is done! Writing aligned sequences to files"
-          let outputFile = "./output/output.txt"
-          writeFile outputFile $ unlines [alignd1, alignd2]
+        -- "row" -> do
+        --   let matrix = row schema seq1 seq2
+        --       (alignd1, alignd2) = traceback matrix seq1 seq2
+        --   putStrLn "Alignment is done! Writing aligned sequences to files"
+        --   let outputFile = "./output/output.txt"
+        --   writeFile outputFile $ unlines [alignd1, alignd2]
+        -- "column" -> do
+        --   let matrix = column schema seq1 seq2
+        --       (alignd1, alignd2) = traceback matrix seq1 seq2
+        --   putStrLn "Alignment is done! Writing aligned sequences to files"
+        --   let outputFile = "./output/output.txt"
+        --   writeFile outputFile $ unlines [alignd1, alignd2]
+        -- "sequential" -> do
+        --   let matrix = sequential schema seq1 seq2
+        --       (alignd1, alignd2) = traceback matrix seq1 seq2
+        --   putStrLn "Alignment is done! Writing aligned sequences to files"
+        --   let outputFile = "./output/output.txt"
+        --   writeFile outputFile $ unlines [alignd1, alignd2]
         _ -> do
           putStrLn "Invalid method specified"
           return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,127 +2,142 @@ module Lib where
 
 import Control.Parallel.Strategies (rpar, parMap)
 import Data.Array (listArray, Array, (!), assocs, bounds, (//), range, array)
+import Data.IORef
+import Control.Monad (forM_)
+
+-- type MutableArray = IORef (Array (Int, Int) Int)
+
+-- data Scoring = Scoring
+--   { matchScore :: Int
+--   , mismatchPenalty :: Int
+--   , gapPenalty :: Int
+--   }
+
+-- -- Define the scoring function
+-- score :: Scoring -> Char -> Char -> Int
+-- score scoring a b
+--   | a == b    = matchScore scoring
+--   | otherwise = -mismatchPenalty scoring
+
+-- initializeMutableArray :: IO MutableArray
+-- initializeMutableArray n = newIORef $ listArray ((0, 0), (n-1, n-1)) $ repeat 0
 
 
-data Scoring = Scoring
-  { matchScore :: Int
-  , mismatchPenalty :: Int
-  , gapPenalty :: Int
-  }
+-- updateMutableArray :: MutableArray -> (Int, Int) -> Int -> IO ()
+-- updateMutableArray mutableArray index newValue = do
+--   oldArray <- readIORef mutableArray
+--   let newArray = oldArray // [(index, newValue)]
+--   writeIORef mutableArray newArray
 
--- Define the scoring function
-score :: Scoring -> Char -> Char -> Int
-score scoring a b
-  | a == b    = matchScore scoring
-  | otherwise = -mismatchPenalty scoring
+-- readIndex :: MutableArray -> (Int, Int) -> IO Int
+-- readIndex mutableArray index = do
+--   currentArray <- readIORef mutableArray
+--   return (currentArray ! index)
 
-antidiagonalIndices :: Int -> [[(Int, Int)]]
-antidiagonalIndices n =
-  [ [(i, k - i) | i <- [0..k], k - i < n, k - i >= 0 && i < n] | k <- [0..2*(n-1)] ]
+-- antidiagonalIndices :: Int -> [[(Int, Int)]]
+-- antidiagonalIndices n =
+--   [ [(i, k - i) | i <- [0..k], k - i < n, k - i >= 0 && i < n] | k <- [0..2*(n-1)] ]
 
-adiagonal :: Scoring -> String -> String -> Array (Int, Int) Int
-adiagonal scoring s1 s2 =
-    let n = length s1
-        m = length s2
-        diagonals = antidiagonalIndices n
+-- adiagonal :: Scoring -> String -> String -> IO MutableArray
+-- adiagonal scoring s1 s2 = do
+--   let n = length s1
+--       m = length s2
+--   mutableArray <- initializeMutableArray n
+--   diagonals <- return $ antidiagonalIndices n
 
-        calculateScore :: (Int, Int) -> Int
-        calculateScore (i, j)
-          | i == 0 = -j * gapPenalty scoring
-          | j == 0 = -i * gapPenalty scoring
-          | otherwise = maximum
-                        [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
-                        , scores ! (i, j-1) - gapPenalty scoring
-                        , scores ! (i-1, j) - gapPenalty scoring
-                        ]
+--   forM_ diagonals $ \diag -> do
+--     scores <- readIORef mutableArray
+--     forM_ diag $ \(i, j) -> do
+--       let newScore
+--             | i == 0 = -j * gapPenalty scoring
+--             | j == 0 = -i * gapPenalty scoring
+--             | otherwise = maximum
+--                             [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
+--                             , scores ! (i, j-1) - gapPenalty scoring
+--                             , scores ! (i-1, j) - gapPenalty scoring
+--                             ]
+--       updateMutableArray mutableArray (i, j) newScore
 
-        scores = listArray ((0, 0), (n-1, m-1)) $ parMap rpar calculateScore (range ((0, 0), (n-1, m-1)))
-
-        updateDiagonal :: Array (Int, Int) Int -> [(Int, Int)] -> Array (Int, Int) Int
-        updateDiagonal arr diag = arr // assocs (listArray (head diag, last diag) $ map (arr !) diag)
-
-        updatedScores = foldl updateDiagonal scores diagonals
-
-    in updatedScores
+--   return mutableArray
 
 
-row :: Scoring -> String -> String -> Array (Int, Int) Int
-row scoring s1 s2 =
-  let n = length s1
-      m = length s2
-      -- Function to calculate score for a cell (i, j)
-      calculateScore i j
-        | i == 0 = -j * gapPenalty scoring
-        | j == 0 = -i * gapPenalty scoring
-        | otherwise = maximum
-                        [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
-                        , scores ! (i, j-1) - gapPenalty scoring
-                        , scores ! (i-1, j) - gapPenalty scoring
-                        ]
+-- row :: Scoring -> String -> String -> Array (Int, Int) Int
+-- row scoring s1 s2 =
+--   let n = length s1
+--       m = length s2
+--       -- Function to calculate score for a cell (i, j)
+--       calculateScore i j
+--         | i == 0 = -j * gapPenalty scoring
+--         | j == 0 = -i * gapPenalty scoring
+--         | otherwise = maximum
+--                         [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
+--                         , scores ! (i, j-1) - gapPenalty scoring
+--                         , scores ! (i-1, j) - gapPenalty scoring
+--                         ]
 
-      -- Compute rows in parallel
-      scores = array ((0, 0), (n, m)) $ do
-        let row i = [((i, j), calculateScore i j) | j <- [0..m]]
-        concat $ parMap rpar row [0..m]
+--       -- Compute rows in parallel
+--       scores = array ((0, 0), (n, m)) $ do
+--         let row i = [((i, j), calculateScore i j) | j <- [0..m]]
+--         concat $ parMap rpar row [0..m]
 
-  in scores
+--   in scores
 
-column :: Scoring -> String -> String -> Array (Int, Int) Int
-column scoring s1 s2 =
-  let n = length s1
-      m = length s2
-      -- Function to calculate score for a cell (i, j)
-      calculateScore i j
-        | i == 0 = -j * gapPenalty scoring
-        | j == 0 = -i * gapPenalty scoring
-        | otherwise = maximum
-                        [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
-                        , scores ! (i, j-1) - gapPenalty scoring
-                        , scores ! (i-1, j) - gapPenalty scoring
-                        ]
+-- column :: Scoring -> String -> String -> Array (Int, Int) Int
+-- column scoring s1 s2 =
+--   let n = length s1
+--       m = length s2
+--       -- Function to calculate score for a cell (i, j)
+--       calculateScore i j
+--         | i == 0 = -j * gapPenalty scoring
+--         | j == 0 = -i * gapPenalty scoring
+--         | otherwise = maximum
+--                         [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
+--                         , scores ! (i, j-1) - gapPenalty scoring
+--                         , scores ! (i-1, j) - gapPenalty scoring
+--                         ]
 
-      -- Compute rows in parallel
-      scores = array ((0, 0), (n, m)) $ do
-        let column j = [((i, j), calculateScore i j) | i <- [0..n]]
-        concat $ parMap rpar column [0..n]
+--       -- Compute rows in parallel
+--       scores = array ((0, 0), (n, m)) $ do
+--         let column j = [((i, j), calculateScore i j) | i <- [0..n]]
+--         concat $ parMap rpar column [0..n]
 
-  in scores
+--   in scores
 
 
-sequential :: Scoring -> String -> String -> Array (Int, Int) Int
-sequential scoring s1 s2 =
-    let n = length s1
-        m = length s2
-        indexes = [(i, j) | i <- [0..n], j <- [0..m]]
+-- sequential :: Scoring -> String -> String -> Array (Int, Int) Int
+-- sequential scoring s1 s2 =
+--     let n = length s1
+--         m = length s2
+--         indexes = [(i, j) | i <- [0..n], j <- [0..m]]
 
-        calculateScore (i, j)
-          | i == 0    = -j * gapPenalty scoring
-          | j == 0    = -i * gapPenalty scoring
-          | otherwise = maximum
-                            [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
-                            , scores ! (i, j-1) - gapPenalty scoring
-                            , scores ! (i-1, j) - gapPenalty scoring
-                            ]
-        scores = listArray ((0, 0), (n, m)) $ map calculateScore indexes
-    in scores
+--         calculateScore (i, j)
+--           | i == 0    = -j * gapPenalty scoring
+--           | j == 0    = -i * gapPenalty scoring
+--           | otherwise = maximum
+--                             [ scores ! (i-1, j-1) + score scoring (s1 !! (i-1)) (s2 !! (j-1))
+--                             , scores ! (i, j-1) - gapPenalty scoring
+--                             , scores ! (i-1, j) - gapPenalty scoring
+--                             ]
+--         scores = listArray ((0, 0), (n, m)) $ map calculateScore indexes
+--     in scores
 
--- Traceback to get the aligned sequences
-traceback :: Array (Int, Int) Int -> String -> String -> (String, String)
-traceback scores s1 s2 = go (n, m) ("", "")
-  where
-    (n, m) = snd $ bounds scores
+-- -- Traceback to get the aligned sequences
+-- traceback :: Array (Int, Int) Int -> String -> String -> (String, String)
+-- traceback scores s1 s2 = go (n, m) ("", "")
+--   where
+--     (n, m) = snd $ bounds scores
 
-    go (i, j) (align1, align2)
-      | i == 0 && j == 0 = (align1, align2)
-      | i > 0 && scores ! (i, j) == scores ! (i-1, j) - 1 =
-        let newAlign1 = s1 !! (i-1) : align1
-            newAlign2 = '-' : align2
-        in go (i-1, j) (newAlign1, newAlign2)
-      | j > 0 && scores ! (i, j) == scores ! (i, j-1) - 1 =
-        let newAlign1 = '-' : align1
-            newAlign2 = s2 !! (j-1) : align2
-        in go (i, j-1) (newAlign1, newAlign2)
-      | otherwise =
-        let newAlign1 = s1 !! (i-1) : align1
-            newAlign2 = s2 !! (j-1) : align2
-        in go (i-1, j-1) (newAlign1, newAlign2)
+--     go (i, j) (align1, align2)
+--       | i == 0 && j == 0 = (align1, align2)
+--       | i > 0 && scores ! (i, j) == scores ! (i-1, j) - 1 =
+--         let newAlign1 = s1 !! (i-1) : align1
+--             newAlign2 = '-' : align2
+--         in go (i-1, j) (newAlign1, newAlign2)
+--       | j > 0 && scores ! (i, j) == scores ! (i, j-1) - 1 =
+--         let newAlign1 = '-' : align1
+--             newAlign2 = s2 !! (j-1) : align2
+--         in go (i, j-1) (newAlign1, newAlign2)
+--       | otherwise =
+--         let newAlign1 = s1 !! (i-1) : align1
+--             newAlign2 = s2 !! (j-1) : align2
+--         in go (i-1, j-1) (newAlign1, newAlign2)
